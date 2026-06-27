ac80bb9223e84e036d8ca95526a169b7c1bcc980
Amu Hlongwane <amu.hlongwane@swelabs.io>
2026-06-27 18:33:01 +0200

    refactor(aop/observability): rename api/provider.ts to api_provider.ts and test to _int_test

--- aop/observability/scm/main/src/saf/index.ts
+++ aop/observability/scm/main/src/saf/index.ts
@@ -1,3 +1,3 @@
-export type { UIObserverContext, ObservabilityProviderConfig, ObservabilityAspect, ObservabilityProvider } from "../api/provider.js"
-export { NoopObserverContext } from "../api/provider.js"
+export type { UIObserverContext, ObservabilityProviderConfig, ObservabilityAspect, ObservabilityProvider } from "../api/api_provider.js"
+export { NoopObserverContext } from "../api/api_provider.js"
 export { DefaultObservabilityProvider } from "../core/default_observability.js"
