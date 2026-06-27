7180031e072bf0cde35e3be81c60b0b1cc877027
Amu Hlongwane <amu.hlongwane@swelabs.io>
2026-06-27 18:33:32 +0200

    refactor(aop/i18n): rename api/provider.ts to api_provider.ts and test to _int_test

--- aop/i18n/scm/main/src/spi/index.ts
+++ aop/i18n/scm/main/src/spi/index.ts
@@ -1,5 +1,6 @@
 import { justjs } from "@justjs/application"
 import { DefaultI18nProvider } from "../core/default_i18n.js"
+import type { I18nProviderConfig } from "../api/api_provider.js"
 
 const provider = new DefaultI18nProvider()
-justjs.providers.register({ concern: "i18n", strategy: "noop", factory: (config?: any) => provider.factory(config) })
+justjs.providers.register({ concern: "i18n", strategy: "noop", factory: (config?: I18nProviderConfig) => provider.factory(config) })
